d106faf497c6da824e5062fbcf60fea6dd498fdc
CharlesHenriquedf <charleshenrique.df@gmail.com>
2022-03-03 16:19:58 -0300

    implementação do token autenticação e middleware.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 require('./controllers/authController')(app);
+require('./controllers/projectContoller')(app);
 
 
-app.listen(3001);
+app.listen(3000);
